02af9904f6284dd4753d458698717625624ace79
Juan Fernando Galvez Parra <jfgalvezparra@gmail.com>
2022-10-19 15:14:00 -0500

    RTK Configurado

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
-import ProductCard from './components/ProductCard';
+import ProductCard from './app/components/ProductCard';
+import { useSelector } from 'react-redux'
 
 function App() {
+  const cartState = useSelector(state => state.cart)
+  console.log(cartState)
   return (
     <div className="App">
       <ProductCard />
--- client/src/app/features/cart/cartSlice.js
+++ client/src/app/features/cart/cartSlice.js
@@ -5,12 +5,9 @@ export const cartSlice = createSlice({
   name: 'cart',
   initialState: [],
   reducers: {
-    addTradicional: (state, action) => {
-      console.log(state, action)
-    }
+    
   }
 })
 
-export const { addTradicional } = cartSlice.actions;
 
 export default cartSlice.reducer;
