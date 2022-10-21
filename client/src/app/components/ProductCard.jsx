43ab5cdf0937caf9920da639682561123e91c02a
Juan Fernando Galvez Parra <jfgalvezparra@gmail.com>
2022-10-20 23:34:29 -0500

    CORS

--- client/src/app/components/ProductCard.jsx
+++ client/src/app/components/ProductCard.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+
+
+
+    <div className="w-full max-w-sm rounded-lg bg-red-600 shadow-md dark:bg-gray-800">
+      <div className='underline'>Product card</div>
+    </div>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
+// Importaciones
 import express, { json } from "express"
 import barbarosRoutes from './routes/barbaros.routes.js'
 import { PORT } from "./config.js";
 import morgan from 'morgan'
 
+// Inicializacion
 const app = express();
 
 // Middlewares
 app.use(morgan('tiny'))
-
-
 app.use(express.json())
 app.use(barbarosRoutes)
 
+// Configuraciones
 app.listen(PORT);
 console.log(`Server started on port ${PORT}`);
 
